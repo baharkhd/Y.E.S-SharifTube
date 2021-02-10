35a7c972a9aaa5b4a9e8c56853afb63f1702dfcf
baharkhd <baharkh127@gmail.com>
2021-02-10 10:55:51 +0330

    changed content page format

--- front/src/pages/Content/ContentPage.js
+++ front/src/pages/Content/ContentPage.js
@@ -5,25 +5,10 @@ import {
   Segment,
   Grid,
   Feed,
-  Icon
+  Icon,
+  Divider
 } from "semantic-ui-react";
-
-// type Comment{
-//     id: ID!
-//     author: User!
-//     body: String!
-//     timestamp: Int!
-//     replies: [Reply!]
-//     content: Content!
-// }
-
-// type Reply{
-//     id: ID!
-//     author: User!
-//     body: String!
-//     timestamp: Int!
-//     comment: Comment!
-// }
+import "./ContentPage.css";
 
 const comments = [
   {
@@ -76,7 +61,88 @@ const comments = [
         timestamp: "11 Feb3"
       }
     ]
-  }
+  },
+  {
+    author: "bahar",
+    body: "hii guys! :)",
+    timestamp: "10 Feb",
+    replies: [
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      }
+    ]
+  },
+  {
+    author: "bahar",
+    body: "hii guys! :)",
+    timestamp: "10 Feb",
+    replies: [
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      }
+    ]
+  },
+  {
+    author: "bahar",
+    body: "hii guys! :)",
+    timestamp: "10 Feb",
+    replies: [
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "David",
+        body: "bye bahar:(",
+        timestamp: "12 Feb"
+      },
+      {
+        author: "khodabakhshian",
+        body: "hi bahar:D",
+        timestamp: "11 Feb"
+      }
+    ]
+  },
 ];
 
 const Reply = ({ author, body, time }) => {
@@ -100,41 +166,57 @@ const Reply = ({ author, body, time }) => {
 function ContentPage(props) {
   return (
     <div>
-      <Segment style={{ top: 70 }}>
-        <Placeholder>
-          <Placeholder.Image rectangular />
-        </Placeholder>
-        <Grid columns={1} style={{ margin: 10 }}>
-          <Grid.Row>
-            <Feed>
-              {comments.map(comment => {
-                return (
-                  <Feed.Event>
-                    <Feed.Label>
-                      <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
-                    </Feed.Label>
-                    <Feed.Content>
-                      <Feed.Summary>
-                        <Feed.User>{comment.author}</Feed.User> {comment.body}
-                        <Feed.Date>{comment.timestamp}</Feed.Date>
-                      </Feed.Summary>
-                      <Feed.Meta>
-                        {comment.replies.map(reply => {
-                          return (
-                            <Reply
-                              author={reply.author}
-                              time={reply.timestamp}
-                              body={reply.body}
-                            />
-                          );
-                        })}
-                      </Feed.Meta>
-                    </Feed.Content>
-                  </Feed.Event>
-                );
-              })}
-            </Feed>
-          </Grid.Row>
+      <Segment style={{ top: 70, overflow: "hidden", borderRadius: 0 }}>
+        <Grid columns={2} textAlign="center" fluid >
+          <Grid.Column>
+            <Placeholder className="test" fluid>
+              <Placeholder.Image rectangular />
+            </Placeholder>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <Feed>
+                {comments.map(comment => {
+                  return (
+                    <Feed.Event>
+                      {/* <Feed.Label>
+                              <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
+                            </Feed.Label> */}
+                      <Feed.Content>
+                        <Feed.Summary>
+                          <Feed.User>{comment.author}</Feed.User> {comment.body}
+                          <Feed.Date>{comment.timestamp}</Feed.Date>
+                        </Feed.Summary>
+                        <Feed.Meta>
+                          {comment.replies.map(reply => {
+                            return (
+                              <Reply
+                                author={reply.author}
+                                time={reply.timestamp}
+                                body={reply.body}
+                              />
+                            );
+                          })}
+                        </Feed.Meta>
+                      </Feed.Content>
+                    </Feed.Event>
+                  );
+                })}
+              </Feed>
+            </Segment>
+          </Grid.Column>
+          {/* <Grid.Row fluid>
+            <Segment >
+              <Placeholder fluid>
+                <Placeholder.Image rectangular />
+              </Placeholder>
+              <Grid columns={1} style={{ margin: 10 }} textAlign="center">
+                <Grid.Row>
+                  
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </Grid.Row> */}
         </Grid>
       </Segment>
     </div>
@@ -142,3 +224,50 @@ function ContentPage(props) {
 }
 
 export default ContentPage;
+
+// <Segment style={{ top: 70 }}>
+//         <Grid columns={1} textAlign="center" fluid className="test">
+//           <Grid.Row fluid>
+//             <Segment >
+//               <Placeholder fluid>
+//                 <Placeholder.Image rectangular />
+//               </Placeholder>
+//               <Grid columns={1} style={{ margin: 10 }} textAlign="center">
+//                 <Grid.Row>
+//                   <Segment>
+//                     <Feed>
+//                       {comments.map(comment => {
+//                         return (
+//                           <Feed.Event>
+//                             {/* <Feed.Label>
+//                               <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
+//                             </Feed.Label> */}
+//                             <Feed.Content>
+//                               <Feed.Summary>
+//                                 <Feed.User>{comment.author}</Feed.User>{" "}
+//                                 {comment.body}
+//                                 <Feed.Date>{comment.timestamp}</Feed.Date>
+//                               </Feed.Summary>
+//                               <Feed.Meta>
+//                                 {comment.replies.map(reply => {
+//                                   return (
+//                                     <Reply
+//                                       author={reply.author}
+//                                       time={reply.timestamp}
+//                                       body={reply.body}
+//                                     />
+//                                   );
+//                                 })}
+//                               </Feed.Meta>
+//                             </Feed.Content>
+//                           </Feed.Event>
+//                         );
+//                       })}
+//                     </Feed>
+//                   </Segment>
+//                 </Grid.Row>
+//               </Grid>
+//             </Segment>
+//           </Grid.Row>
+//         </Grid>
+//       </Segment>
