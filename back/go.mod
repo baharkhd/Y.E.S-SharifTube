a42d7a528a126e5d7ae229fb1f40202f24d95f97
ArshiA Akhavan <letmemakenewone@gmail.com>
2021-02-10 07:53:28 +0330

    using gin

--- back/go.mod
+++ back/go.mod
@@ -5,7 +5,9 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/vektah/gqlparser/v2 v2.1.0
 	go.mongodb.org/mongo-driver v1.4.6
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
 )
--- back/server.go
+++ back/server.go
@@ -1,34 +1,62 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"yes-sharifTube/graph"
-	"yes-sharifTube/graph/generated"
-	"yes-sharifTube/internal/model/user"
-	"yes-sharifTube/pkg/database/mongodb"
-
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"yes-sharifTube/graph"
+	"yes-sharifTube/graph/generated"
+	"yes-sharifTube/internal/middleware/auth"
+	"yes-sharifTube/internal/middleware/ggcontext"
+	"yes-sharifTube/internal/model/user"
+	"yes-sharifTube/pkg/database/mongodb"
 )
 
 const defaultPort = "8080"
+const queryComplexity = 8
 
 func main() {
+	//setting a mongodb driver for DBDriver filed of our controllers instance
+	user.UserDBD = mongodb.NewUserMongoDriver("yes-blog", "users")
 
-	user.UserDBD=mongodb.NewUserMongoDriver("yes-blog", "users");
+	// Setting up Gin
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowHeaders: []string{auth.AuthHeaderKey,
+			"content-type"},
+		AllowCredentials: true,
+	}))
+	r.Use(ggcontext.GinContextToContextMiddleware())
+	r.Use(auth.Middleware())
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	// routing
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
 
+	//let it begin
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	r.Run(":" + defaultPort)
+
+}
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	//srv.Use(extension.FixedComplexityLimit(queryComplexity))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	return func(c *gin.Context) {
+		srv.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	pg := playground.Handler("Yes-blog playground", "/query")
+
+	return func(c *gin.Context) {
+		pg.ServeHTTP(c.Writer, c.Request)
+	}
 }
